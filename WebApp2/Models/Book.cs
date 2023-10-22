22bc9f87e589ab63b65b95f79dd86f9537ef21d3
sergiuavram <contact@sergiuavram.com>
2023-10-22 16:12:30 +0300

    first commit

---
+++ WebApp2/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace WebApp2.Models;
+
+public class Book
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public decimal Price { get; set; }
+}
