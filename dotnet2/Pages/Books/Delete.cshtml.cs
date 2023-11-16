ea71d352daa85dcf5a27013d3ad043ba0c9c2715
sergiuavram <contact@sergiuavram.com>
2023-11-16 19:26:34 +0200

    code updated

--- dotnet2/Pages/Books/Delete.cshtml.cs
+++ dotnet2/Pages/Books/Delete.cshtml.cs
@@ -29,7 +29,7 @@ namespace dotnet2.Pages.Books
                 return NotFound();
             }
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            var book = await _context.Book.Include(x => x.Author).FirstOrDefaultAsync(m => m.ID == id);
 
             if (book == null)
             {
